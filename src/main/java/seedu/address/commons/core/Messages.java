3c83499ab9fea349e7f5a27ab366558da81a7d7b
jeong-jaeho <e0960435@u.nus.edu>
2024-03-07 14:51:21 +0800

    Update the correct Messages

--- src/main/java/seedu/address/commons/core/Messages.java
+++
@@ -1,9 +0,0 @@
-package seedu.address.commons.core;
-
-public class Messages {
-    // Other message constants...
-
-    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! %1$s";
-
-    // Other message constants...
-}
