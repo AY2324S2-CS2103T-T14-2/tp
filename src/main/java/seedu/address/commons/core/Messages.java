3e19cce7f1d234c72f99fed021de6b613dd9edbe
jeong-jaeho <e0960435@u.nus.edu>
2024-03-07 14:37:21 +0800

    Add Remark command

---
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -0,0 +1,9 @@
+package seedu.address.commons.core;
+
+public class Messages {
+    // Other message constants...
+
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! %1$s";
+
+    // Other message constants...
+}
